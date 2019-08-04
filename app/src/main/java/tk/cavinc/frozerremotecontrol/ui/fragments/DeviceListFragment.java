ba12d1ab0bfc8ec867b0952170ed095c121b5f74
CavInc <cavinc20@gmail.com>
2019-08-04 19:30:05 +0700

    Перелано

--- app/src/main/java/tk/cavinc/frozerremotecontrol/ui/fragments/DeviceListFragment.java
+++ app/src/main/java/tk/cavinc/frozerremotecontrol/ui/fragments/DeviceListFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
@@ -52,6 +54,8 @@ public class DeviceListFragment extends Fragment {
 
         //swipeDetector = new SwipeDetector();
         //mListView.setOnTouchListener(swipeDetector);
+        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        actionBar.setSubtitle("");
 
         return rootView;
     }
