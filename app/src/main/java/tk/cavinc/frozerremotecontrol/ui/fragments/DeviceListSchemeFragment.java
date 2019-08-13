a55d06d154ee5da28224f2b0c93b312a43db5cdd
CavInc <cavinc20@gmail.com>
2019-08-14 00:25:32 +0700

    Работа над схемой

--- app/src/main/java/tk/cavinc/frozerremotecontrol/ui/fragments/DeviceListSchemeFragment.java
+++ app/src/main/java/tk/cavinc/frozerremotecontrol/ui/fragments/DeviceListSchemeFragment.java
@@ -1,8 +1,33 @@
 package tk.cavinc.frozerremotecontrol.ui.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tk.cavinc.frozerremotecontrol.R;
+import tk.cavinc.frozerremotecontrol.data.managers.DataManager;
+
 /**
  * Created by cav on 13.08.19.
  */
 
-public class DeviceListSchemeFragment {
+public class DeviceListSchemeFragment extends Fragment {
+    private DataManager mDataManager;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.device_scheme_fragment, container, false);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
