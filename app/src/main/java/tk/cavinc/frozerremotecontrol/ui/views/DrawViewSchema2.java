771815d940227c03fbf5ef4b082d1d2725497dd1
CavInc <cavinc20@gmail.com>
2019-08-16 00:50:12 +0700

    Эсперименты

--- app/src/main/java/tk/cavinc/frozerremotecontrol/ui/views/DrawViewSchema2.java
+++ app/src/main/java/tk/cavinc/frozerremotecontrol/ui/views/DrawViewSchema2.java
@@ -7,10 +7,14 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Picture;
+import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 import tk.cavinc.frozerremotecontrol.R;
 
@@ -22,6 +26,8 @@ public class DrawViewSchema2 extends View {
 
     private Paint mPaint = new Paint();
     private Bitmap mFr1;
+    private int width;
+    private int heigth;
 
     public DrawViewSchema2(Context context) {
         super(context);
@@ -29,7 +35,19 @@ public class DrawViewSchema2 extends View {
 
     public DrawViewSchema2(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initWidthAndHeigth(context);
         mFr1 = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_fr1);
+        Drawable mFr11 = context.getResources().getDrawable(R.drawable.ic_fr1);
+
+    }
+
+    private void initWidthAndHeigth(Context context) {
+        WindowManager windowManager= (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display=windowManager.getDefaultDisplay();
+        Point point= new Point();
+        display.getSize(point);
+        width=point.x;
+        heigth=point.y;
     }
 
     //http://www.cyberforum.ru/android-dev/thread2122081.html
@@ -47,5 +65,13 @@ public class DrawViewSchema2 extends View {
         Rect rect = new Rect();
         rect.set(250, 300, 350, 500);
         canvas.drawRect(rect,mPaint);
+
+        mPaint.setColor(Color.argb(100,200,255,255));
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(4);
+        canvas.drawRect(10,200,50,220,mPaint);
+
     }
+
+
 }
